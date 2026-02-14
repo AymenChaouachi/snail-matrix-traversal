6e2a82520496fbf8798c80bd97e1442b27297920
AymenChaouachi <aymen.12.2002@gmail.com>
2026-02-14 23:37:47 +0100

    Snail Matrix Traversal submission

---
+++ solution.py
@@ -0,0 +1,47 @@
+import sys
+
+def snail(matrix):
+    result = []
+    n = len(matrix)
+
+    top, bottom = 0, n - 1
+    left, right = 0, n - 1
+
+    while top <= bottom and left <= right:
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+
+        if top <= bottom:
+            for i in range(right, left - 1, -1):
+                result.append(matrix[bottom][i])
+            bottom -= 1
+
+        if left <= right:
+            for i in range(bottom, top - 1, -1):
+                result.append(matrix[i][left])
+            left += 1
+
+    return result
+
+
+if __name__ == "__main__":
+    input = sys.stdin.read
+    data = input().strip().split()
+
+    n = int(data[0])
+    nums = list(map(int, data[1:]))
+
+    matrix = []
+    index = 0
+    for _ in range(n):
+        row = nums[index:index+n]
+        matrix.append(row)
+        index += n
+
+    result = snail(matrix)
+    print(*result)
