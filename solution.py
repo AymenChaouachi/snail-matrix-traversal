77b3fa9783ca7072ce0d5fdb48b84093f7d397d2
AymenChaouachi <aymen.12.2002@gmail.com>
2026-02-15 00:57:36 +0100

    Generate snail matrix dynamically from empty matrix

--- solution.py
+++ solution.py
@@ -1,47 +1,40 @@
-def snail(matrix):
-    result = []
-    n = len(matrix)
+def generate_snail(n):
+    matrix = [[0] * n for _ in range(n)]
 
     top, bottom = 0, n - 1
     left, right = 0, n - 1
+    num = 1
 
     while top <= bottom and left <= right:
         for i in range(left, right + 1):
-            result.append(matrix[top][i])
+            matrix[top][i] = num
+            num += 1
         top += 1
 
         for i in range(top, bottom + 1):
-            result.append(matrix[i][right])
+            matrix[i][right] = num
+            num += 1
         right -= 1
 
         if top <= bottom:
             for i in range(right, left - 1, -1):
-                result.append(matrix[bottom][i])
+                matrix[bottom][i] = num
+                num += 1
             bottom -= 1
 
         if left <= right:
             for i in range(bottom, top - 1, -1):
-                result.append(matrix[i][left])
+                matrix[i][left] = num
+                num += 1
             left += 1
 
-    return result
+    return matrix
 
 
 if __name__ == "__main__":
-    matrix = [
-        [1,  2,  3,  4,  5],
-        [16, 17, 18, 19, 6],
-        [15, 24, 25, 20, 7],
-        [14, 23, 22, 21, 8],
-        [13, 12, 11, 10, 9]
-    ]
+    n = 5  
 
-    result = snail(matrix)
+    matrix = generate_snail(n)
 
-    n = len(matrix)
-    index = 0
-
-    for _ in range(n):
-        row = result[index:index+n]
+    for row in matrix:
         print(" ".join(f"{num:2}" for num in row))
-        index += n
